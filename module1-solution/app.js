fd3635eeb7429af5e927ae8cbda7748e21ee594c
larzon <lana.arzon@gmail.com>
2021-10-04 18:39:41 +0200

    Add app with dummy controller

---
+++ module1-solution/app.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+MsgController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  // TODO
+}
+
+})();
